e4c3d0001d00791f9b8d16c98355581f2ccf39db
grasloken <gabriel.j.persson@gmail.com>
2016-12-12 19:03:01 +0100

    Changed parsing method (new structure)
    Not working properly yet

--- CDIO_CC3200/CDIO_CC3200.c
+++ CDIO_CC3200/CDIO_CC3200.c
@@ -43,11 +43,12 @@
 /* Example/Board Header files */
 #include "Board.h"
 
-#define TASKSTACKSIZE   10000
+#define TASKSTACKSIZE   100000
 #define QUAD 	1
 #define DIR2	8
 #define SCALE 	1
-#define COORDSIZE 14
+#define COORDSIZE 10000
+#define COMMANDSIZE 6
 #define MAXDUTY 1650
 #define MINDUTY 800
 #define MAXSTEPSY 100000 // Set correct value after measurements
@@ -63,6 +64,14 @@ enum directions {
 struct step{
 	int x;
 	int y;
+	int xScale;
+	int yScale;
+};
+
+struct command{
+	short int x;
+	short int y;
+	char pen;
 };
 
 /* GLOBAL VARABLES */
@@ -76,8 +85,9 @@ Char task0Stack[TASKSTACKSIZE];
 void brytarIntY(unsigned int index);
 void brytarIntX(unsigned int index);
 static void move(unsigned char direction);
+void read_file(struct command coord[], UART_Handle uart, unsigned int *addedCommands);
 void moveToOrigin();
-void charToInt(char coord[], int *xStart, int *yStart, int *xEnd, int *yEnd, int *zPos);
+void charToInt(char temp[], short int *xCoord, short int *yCoord);
 
 void brytarIntY(unsigned int index)
 {
@@ -112,26 +122,17 @@ void moveTask(UArg arg0, UArg arg1)
 	PWM_Params params;
 
 	// Get port and pins
-	unsigned int GPIO7Port = 0; // Direction right motor
-	unsigned char GPIO7Pin; 	// Direction right motor
 	unsigned int GPIO9Port = 0; // PWM (Also LED)
 	unsigned char GPIO9Pin;		// PWM (Also LED)
-	unsigned int GPIO10Port = 0;// Step
-	unsigned char GPIO10Pin;	// Step
-	unsigned int GPIO11Port = 0;// Direction left motor
-	unsigned char GPIO11Pin;	// Direction left motor
-	GPIO_IF_GetPortNPin(7, &GPIO7Port, &GPIO7Pin); // Direction right motor
 	GPIO_IF_GetPortNPin(9, &GPIO9Port, &GPIO9Pin); // PWM (Also LED)
-	GPIO_IF_GetPortNPin(10, &GPIO10Port, &GPIO10Pin); // Step
-	GPIO_IF_GetPortNPin(11, &GPIO11Port, &GPIO11Pin); // Direction right motor
 
-	int xStart, xEnd, yStart, yEnd, zPos = 0;
 	steps.x = 0;
 	steps.y = 0;
-	char msg;
-	char coord[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	unsigned int addedCommands = 0;
+	//char coord[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	struct command coord[COORDSIZE];
 
-	GPIO_IF_LedOff(MCU_ALL_LED_IND);
+	//GPIO_IF_LedOff(MCU_ALL_LED_IND);
 
 	/* Initiate the PWM */
 	uint16_t   pwmPeriod = 20000;      // Period and duty in microseconds
@@ -164,184 +165,122 @@ void moveTask(UArg arg0, UArg arg1)
 	const char Prompt[] = "\fHeeeello!\r\n";
 	UART_write(uart, Prompt, sizeof(Prompt));
 
-	int i = 0;
+	read_file(coord,uart, &addedCommands);
+
+	int nCommands; // Iterator for coord
     while (1)
     {
-    	// Fetch data (get coordinate)
-    	UART_read(uart, &msg, 1);
-
-
-    	// Decode data (read parameters from coordinate data)
-    	switch(msg)
+    	for(nCommands = 0; nCommands < addedCommands; nCommands++) // Step through all commands in coord
     	{
-    		case 'c':
-    			i = 0;
-    			while(i <= 13)
-    			{
-    				UART_read(uart, &msg, 1); // Read message
-
-    				// Read
-					coord[i] = msg;
-
-					i++;
-    			}
-
-
-    			charToInt(coord, &xStart, &yStart, &xEnd, &yEnd, &zPos);
-
-    			// Move to start coordinate
-    			while((steps.x != xStart) || (steps.y != yStart))
-    			{
-    				if(steps.x < xStart)
-    				{
-    					move(POS_X);
-    				}
-    				else if (steps.x > xStart)
-    				{
-    					move(NEG_X);
-    				}
-
-    				if ((steps.x != xStart) && (steps.y != yStart))
-    				{
-    					Task_sleep(20);
-    					//MAP_UtilsDelay(20000);
-    				}
-
-    				if(steps.y < yStart)
-    				{
-    					move(POS_Y);
-    				}
-    				else if (steps.y > yStart)
-    				{
-    					move(NEG_Y);
-    				}
-    			}
-
-    			// Move from start to end coordinate
-    			while((steps.x != xEnd) || (steps.y != yEnd))
+			// Move to start coordinate
+			while((steps.x != coord[nCommands].x) || (steps.y != coord[nCommands].y))
+			{
+				if(steps.x < coord[nCommands].x)
 				{
-					if(steps.x < xEnd)
-					{
-						move(POS_X);
-					}
-					else if (steps.x > xEnd)
-					{
-						move(NEG_X);
-					}
-
-					if ((steps.x != xEnd) && (steps.y != yEnd))
-					{
-						Task_sleep(20);
-						//MAP_UtilsDelay(20000);
-					}
-
-					if(steps.y < yEnd)
-					{
-						move(POS_Y);
-					}
-					else if (steps.y > yEnd)
-					{
-						move(NEG_Y);
-					}
+					move(POS_X);
+				}
+				else if (steps.x > coord[nCommands].x)
+				{
+					move(NEG_X);
 				}
 
-
-    			/*
-    			GPIO_IF_Set(9, GPIO9Port, GPIO9Pin, 1); // LED on
-
-    			GPIO_IF_Set(11, GPIO11Port, GPIO11Pin, 1); // Direction for left driver GP11 - pin 2
-				MAP_UtilsDelay(20000);
-				GPIO_IF_Set(10, GPIO10Port, GPIO10Pin, 1); // Step - Pin 1 GP10
-    			MAP_UtilsDelay(40000);
-    			GPIO_IF_Set(10, GPIO10Port, GPIO10Pin, 0); // Step - Pin 1 GP10
-				*/
-    			/*
-    			while(msg != '\n')
-    			{
-    				UART_read(uart, &msg, 1);
-    				UART_write(uart, &msg, 1);
-    				//coord[i] = msg;
-    				i++;
-    			}
-    			*/
-    			break;
-
-    		case 'x':
-    			GPIO_IF_Set(9, GPIO9Port, GPIO9Pin, 0); // LED off
-
-    			GPIO_IF_Set(11, GPIO11Port, GPIO11Pin, 0); // Direction for left driver GP11 - pin 2
-    			MAP_UtilsDelay(20000);
-    			GPIO_IF_Set(10, GPIO10Port, GPIO10Pin, 1); // Step - Pin 1 GP10
-    			MAP_UtilsDelay(40000);
-    			GPIO_IF_Set(10, GPIO10Port, GPIO10Pin, 0); // Step - Pin 1 GP10
-
-    			break;
-
-    		case 'z':
-    			PWM_setDuty(pwm1, duty);
-
-    			duty = (duty + dutyInc);
-				if (duty >= MAXDUTY || duty <= MINDUTY) {
-					dutyInc = - dutyInc;
+				if ((steps.x != coord[nCommands].x) && (steps.y != coord[nCommands].y)) // Only sleep if diagonal movement
+				{
+					Task_sleep(20);
+					//MAP_UtilsDelay(20000);
 				}
 
-				MAP_UtilsDelay(40000);
-
-    			break;
-
-    		case 'v':
-
-    			//POS_Y
-    			GPIO_IF_Set(7, GPIO7Port, GPIO7Pin, 1); // Direction for right driver GP7 - pin 62
-				GPIO_IF_Set(11, GPIO11Port, GPIO11Pin, 0); // Direction for left driver GP11 - pin 2
-
-			    MAP_UtilsDelay(5000);
-				GPIO_IF_Set(10, GPIO10Port, GPIO10Pin, 1); // Step on - Pin 1 GP10
-				MAP_UtilsDelay(5000);
-				GPIO_IF_Set(10, GPIO10Port, GPIO10Pin, 0); // Step off - Pin 1 GP10
-
-    			break;
-
-    		case 'o':
-    			moveToOrigin();
-
-    			break;
-
-    		default:
-    			break;
+				if(steps.y < coord[nCommands].y)
+				{
+					move(POS_Y);
+				}
+				else if (steps.y > coord[nCommands].y)
+				{
+					move(NEG_Y);
+				}
+			}
     	}
     }
+}
+
+void read_file(struct command coord[], UART_Handle uart, unsigned int *addedCommands)
+{
+	char msg;
+	unsigned int nCommands = 0;
+	char temp[COMMANDSIZE];
+	int i;
+
+	UART_read(uart, &msg, 1);
+
+	while((int) msg != EOF)
+	{
+		switch (msg)
+		{
+			case 'U': // Character for pen up
+
+				for(i = 0; i < COMMANDSIZE; i++) // Read input chars into temp array
+				{
+					UART_read(uart, &msg, 1);
+					temp[i] = msg;
+				}
+
+				charToInt(temp, &coord[nCommands].x, &coord[nCommands].y); // Convert and scale chars to x and y coordinates in short ints
+				coord[nCommands].pen = 'U';
+				nCommands++; // Increase number of commands in coord
+				break;
+
+			case 'N': // Character for pen down
+
+				for(i = 0; i < COMMANDSIZE; i++) // Read input chars into temp array
+				{
+					UART_read(uart, &msg, 1);
+					temp[i] = msg;
+				}
+				charToInt(temp, &coord[nCommands].x, &coord[nCommands].y); // Convert and scale chars to x and y coordinates in short ints
+				coord[nCommands].pen = 'N';
+				nCommands++; // Increase number of commands in coord
+				break;
+
+			case 'O':
+					moveToOrigin();
+				break;
+
+			default:
+				break;
+
+		}
+		if (nCommands < COORDSIZE)
+		{
+			UART_read(uart, &msg, 1);
+		}
+		else break;
+	}
+	(*addedCommands) = nCommands; // How many commands added into coord
 }
 
 
 void moveToOrigin()
 {
 	/* Move to the origin */
-	while(!originFlagX)
+	while(!originFlagX) // Move until not in the origin
 	{
-		//if(!originFlagX)
-		//{
-			move(NEG_X);
-		//}
+		move(NEG_X);
 	}
 
-	while(!originFlagY)
+	while(!originFlagY) // Move until not in the origin
 	{
-		//if(!originFlagY)
-		//{
-			move(NEG_Y);
-		//}
+		move(NEG_Y);
 	}
 }
 
-void charToInt(char coord[], int *xStart, int *yStart, int *xEnd, int *yEnd, int *zPos)
+void charToInt(char temp[], short int *xCoord, short int *yCoord)
 {
 	int i;
-	for(i = 0; i < COORDSIZE; i++)
+	for(i = 0; i < COMMANDSIZE; i++)
 	{
-		if ((coord[i] < 58) && (coord[i] > 47))
+		if ((temp[i] < 58) && (temp[i] > 47))
 		{
-			coord[i] = coord[i]-48;
+			temp[i] = temp[i]-48;
 		}
 		else
 		{
@@ -350,17 +289,12 @@ void charToInt(char coord[], int *xStart, int *yStart, int *xEnd, int *yEnd, int
 	}
 
 	// Decode the message coordinates to ints
-	*xStart = 100*coord[0]+10*coord[1]+coord[2];
-	*yStart = 100*coord[3]+10*coord[4]+coord[5];
-	*xEnd = 100*coord[6]+10*coord[7]+coord[8];
-	*yEnd = 100*coord[9]+10*coord[10]+coord[11];
-	*zPos = 10*coord[12]+coord[13];
+	*xCoord = 100*temp[0]+10*temp[1]+temp[2];
+	*yCoord = 100*temp[3]+10*temp[4]+temp[5];
 
 	// Scale millimeters to steps of the motors
-	*xStart = (*xStart)*SCALE;
-	*yStart = (*yStart)*SCALE;
-	*xEnd = (*xEnd)*SCALE;
-	*yEnd = (*yEnd)*SCALE;
+	*xCoord = (*xCoord)*SCALE;
+	*yCoord = (*yCoord)*SCALE;
 }
 
 
